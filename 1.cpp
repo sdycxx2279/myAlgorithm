d9bb007c1eff6ab0c35928a2be8509b92263f633
xuxiao <260964322@qq.com>
2018-03-21 09:13:49 +0800

    create project

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 26096 on 2018/3/20.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 0;
+    for(i = 0; i < 4;i++){
+        cout<< i <<endl;
+    }
+    cout << "Hello, 1!" << endl;
+    return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 0;
+    for(i = 0; i < 4;i++){
+        cout<< i <<endl;
+    }
+    cout << "Hello, World!" << endl;
+    return 0;
+}
