6c5cf6e346181610d5f026b1cbe33dfc8545f875
xuxiao <260964322@qq.com>
2018-03-24 00:53:19 +0800

    ac Question3

--- 3.cpp
+++ 3.cpp
@@ -2,4 +2,36 @@
 //@author Xiao Xu
 //@create 2018-03-22 9:17
 //
+#include<iostream>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLength = 0;
+        int positionOfBegin = 0;
+        set<char> allCharSet;
+        for(int i=0;i<s.length();i++){
+            if(allCharSet.find(s[i]) != allCharSet.end()){
+                maxLength = max(maxLength,i - positionOfBegin);
+                positionOfBegin = s.find(s[i],positionOfBegin)+1;
+                allCharSet.clear();
+                for(int j = positionOfBegin;j<i;j++)
+                    allCharSet.insert(s[j]);
+            }
+            allCharSet.insert(s[i]);
+        }
+        int i = s.length();
+        maxLength = max(maxLength,i - positionOfBegin);
+        return maxLength;
+    }
+};
+
+int main(){
+    Solution solution;
+    string s = "abcabcbb";
+    cout<<solution.lengthOfLongestSubstring(s);
+    return 0;
+}
 
--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
+
 
 int main() {
-    ListNode l = ListNode(1);
-    ListNode* a = &l;
-    a->val = 1;
-    if(a->next)
-        cout<<"a";
-    else
-        cout<<"b";
+    string s = "sssss";
+    cout<<s.length();
     return 0;
 }
